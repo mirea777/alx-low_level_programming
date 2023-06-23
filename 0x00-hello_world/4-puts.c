56421c7a4403086b0860ab2e632505bd4979de77
mouna <m.naamane01@gmail.com>
2023-06-23 10:29:30 -0700

    main 0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
-#include <stdio.h>
-int main(void){
+#include <stdio.h> 
+
+/**
+ * main - Entry point
+ * Description: A C program tha print with put function
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
     puts("\"Programming is like building a multilingual puzzle");
-    return(0)
+    return(0);
 }
